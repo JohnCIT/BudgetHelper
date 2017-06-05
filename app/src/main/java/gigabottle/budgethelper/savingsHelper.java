bf756668af54f1623f8a4dac21355b49c165f1d4
JohnCIT <john.murphy7@mycit.ie>
2017-06-05 01:06:49 +0100

    First commit
    Very first commit for budgetHelper App

---
+++ app/src/main/java/gigabottle/budgethelper/savingsHelper.java
@@ -0,0 +1,13 @@
+package gigabottle.budgethelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class savingsHelper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_savings_helper);
+    }
+}
