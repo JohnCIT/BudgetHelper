1f00f70a6e29c2a4769482d54c0504a30ba53a7d
JohnCIT <john.murphy7@mycit.ie>
2017-06-05 16:26:36 +0100

    Sorted out the horrible layout, still some issues, but much neater and you can now scroll so you know, more useable. Added outlkines for classes too.

---
+++ app/src/main/java/controller/SavingsPageController.java
@@ -0,0 +1,10 @@
+package controller;
+
+/**
+ * Created by John on 05/06/2017.
+ *
+ * Here we will get the instance of the button from layout and add action handlers to them. I imagine
+ * the actions wil go to the logic layers and get info back. But we will see....
+ */
+public class SavingsPageController {
+}
---
+++ app/src/main/java/logic/helpers/LogicHelper.java
@@ -0,0 +1,10 @@
+package logic.helpers;
+
+/**
+ * Created by John on 05/06/2017.
+ *
+ * Here methods will go which will be used by the controller to get for example the average amount
+ * of money to be saved per time periuod to reach their target and so. Note: For moneym use big decimal.
+ */
+public class LogicHelper {
+}
